dc49d25f93ff3dc223a97d163f5ee07a3e042b1e
tnum <teenum@susumu.co.uk>
2017-01-21 20:38:56 -0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
-/**
-* Put your JavaScript here
-*/
+document.body.onload = addTodo;
+
+let state = {
+  todos:[
+    {title: 'First todo', complete: false},
+    {title: 'Second todo', complete: true},
+    {title: 'Third todo', complete: false},
+  ],
+}
+
+function addTodo(){
+  let item = state["todos"];
+  for(var key in item){
+    let newLi = document.createElement("li");
+    if(item[key].complete === true) { 
+      newLi.className = "todo--complete";
+    }
+    let todotitle = item[key].title;
+    let newTodo = document.createTextNode(`Hi! ${todotitle}`);
+    newLi.appendChild(newTodo);
+    let todoList = document.getElementById('todos');
+    todoList.appendChild(newLi);
+  }
+}
 
 console.log( 'Open the dev console to see me. :-)' );
 
